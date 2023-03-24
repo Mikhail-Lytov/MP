3510426ac0e156f7ee15e2ff0ac37b9aa89978c7
Lytov <misha.lytov@yande.xru>
2023-03-24 09:42:41 +0300

    delete init, destroy

--- lab_3_prof/main.py
+++ lab_3_prof/main.py
@@ -1,23 +1,29 @@
+import gc
+
+
 class BTreeNode:
     def __init__(self, t, leaf):
         self.t = t
-        self.leaf = leaf
+        self.leaf = leaf # проверки на лист
         self.keys = []
         self.values = []
         self.children = []
+    def __del__(self):
+        print("object Node destroyed")
 
-class BTreeMap:
+class BTreeMap: # ассациативного массив
     def __init__(self, t):
+        print("Object init")
         self.t = t
         self.root = None
 
-    def get(self, key):
+    def get(self, key): # возращает значение по ключу
         if self.root is None:
             return None
         else:
-            return self._get(key, self.root)
+            return self.__get(key, self.root)
 
-    def _get(self, key, node):
+    def __get(self, key, node): # поиск по ключу
         i = 0
         while i < len(node.keys) and key > node.keys[i]:
             i += 1
@@ -26,9 +32,9 @@ class BTreeMap:
         elif node.leaf:
             return None
         else:
-            return self._get(key, node.children[i])
+            return self.__get(key, node.children[i])
 
-    def set(self, key, value):
+    def set(self, key, value): # добавление элемента в дерево
         if self.root is None:
             self.root = BTreeNode(self.t, True)
             self.root.keys.append(key)
@@ -41,9 +47,9 @@ class BTreeMap:
                 self._split_child(new_root, 0, node)
                 node = new_root
                 self.root = new_root
-            self._set(key, value, node)
+            self.__set(key, value, node)
 
-    def _set(self, key, value, node):
+    def __set(self, key, value, node):
         i = 0
         while i < len(node.keys) and key > node.keys[i]:
             i += 1
@@ -55,9 +61,9 @@ class BTreeMap:
                 self._split_child(node, i, node.children[i])
                 if key > node.keys[i]:
                     i += 1
-            self._set(key, value, node.children[i])
+            self.__set(key, value, node.children[i])
 
-    def _split_child(self, parent, i, child):
+    def _split_child(self, parent, i, child): #перестройка дерева
         new_child = BTreeNode(child.t, child.leaf)
         parent.children.insert(i + 1, new_child)
         parent.keys.insert(i, child.keys[self.t - 1])
@@ -70,7 +76,7 @@ class BTreeMap:
             new_child.children = child.children[self.t:]
             child.children = child.children[:self.t]
 
-    def update(self, key, value):
+    def update(self, key, value): # изменение значения ключа
         node = self.root
         while node is not None:
             i = 0
@@ -82,13 +88,36 @@ class BTreeMap:
             node = node.children[i]
         raise KeyError(key)
 
+    def check_emptiness(self):
+        node = self.root
+        while node is not None:
+            return False
+        return True
+
+    def delete_all(self):
+        self.root = None
+        gc.collect()#собирает полный мусор
+        gc.garbage
+
+    def __del__(self):
+        print("Object destroyed")
+
 
 btree_map = BTreeMap(2)
-btree_map.set(1, "one")
-btree_map.set(2, "two")
 btree_map.set(3, "three")
-print(btree_map.get(1)) # выводит "one"
-print(btree_map.get(2)) # выводит "two"
-print(btree_map.get(3)) # выводит "three"
-btree_map.update(2, "new two")
-print(btree_map.get(2)) # выводит "new two"
+btree_map.set(2, "two")
+btree_map.set(4, "four")
+btree_map.set(5, "five")
+btree_map.set(1, "one")
+print(btree_map.get(1))
+print(btree_map.get(2))
+print(btree_map.get(3))
+btree_map.update(2, "FILE")
+print(btree_map.get(2))
+print()
+print("Список пуст? " + str(btree_map.check_emptiness()))
+print("количество объектов до удаления в мусорке")
+print(btree_map.delete_all())
+print("Список пуст? " + str(btree_map.check_emptiness()))
+print("количество элементов после освобожодения")
+print(btree_map.delete_all())
