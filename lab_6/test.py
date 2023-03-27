987a56fccd4745d42b37cc3861e776d649e0ebc1
Lytov <misha.lytov@yande.xru>
2023-03-27 11:42:08 +0300

    Knuth_Morris_Pratt

--- lab_6/test.py
+++ lab_6/test.py
@@ -5,6 +5,14 @@ def test():
     dictList = [13]
     if collections.Counter(substring.BoyerMoore(line="персональные данные", image="данные")) == collections.Counter(dictList):
         print("BoyerMoore: " + str(True))
+    else:
+        print("BoyerMoore: " + str(False))
+
+    dictList = [2, 8]
+    if substring.Knuth_Morris_Pratt("ABCABAABCABAC", "CAB") == dictList:
+        print("Knuth_Morris_Pratt: " + str(True))
+    else:
+        print("Knuth_Morris_Pratt: " + str(False))
 
 
 
