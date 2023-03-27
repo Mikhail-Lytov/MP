4b43cf3a0ed55b95ce8dead3f9cb08f9dcb23fb1
Lytov <misha.lytov@yande.xru>
2023-03-27 11:23:17 +0300

    version_2

--- lab_6/substring.py
+++ lab_6/substring.py
@@ -40,11 +40,11 @@ def BoyerMoore(line = "", image = ""):
     return occurrencesList
 
 
-def Knuth_Morris_Pratt(line = "", image = ""):
+def prefix(line = "", image = ""):
     image_list = []
     dict = {}
     size = 0
-    size_list = []
+    maxSize = 0
     for i in image:
         if dict.get(i) is None:
             dict[i] = size;
@@ -53,6 +53,7 @@ def Knuth_Morris_Pratt(line = "", image = ""):
             image_list.append(i)
             left_image = []
             right_image = []
+            list_image = []
             size_image = 1;
             for j in image_list[0:len(image_list) - 1]:
                 left_image.append(j)
@@ -61,11 +62,38 @@ def Knuth_Morris_Pratt(line = "", image = ""):
                 print(right_image)
                 size_image += 1
                 if(left_image == right_image):
-                    size += 1
+                    if(len(list_image) == 0):
+                        list_image.append(''.join(left_image))
+                        size = len(left_image)
+                        if size > maxSize: maxSize = size
+                    else:
+                        list_image[0] = ''.join(left_image)
+                        size = len(left_image)
             if(size != 0):
-                dict[i] = size
+                dict[list_image[0]] = size
             size = 0
-    return
+    return maxSize, dict
+
+def Knuth_Morris_Pratt(line='', image=''):
+    i = len(image) - 1
+    str_image = ''
+    flag = False
+    maxSize, dict = prefix(line, image)
+    while i < len(line):
+        j = len(image) - 1
+        if(image[j] == line[i] and j > -1):
+            str_image = str(image[j]) + str_image
+            j -= 1
+            i -= 1
+            flag = True
+        elif j == 0:
+            print("шей")
+        elif flag:
+            if(dict.get(str_image) != None):
+                i += dict.get(str_image)
+            else:
+                i += len(image)-1
+        else: i += len(image) - 1
 
 if __name__ == '__main__':
-    print(Knuth_Morris_Pratt(line="персональные данные", image="abcabd"))
+    print(Knuth_Morris_Pratt(line="abcabeabcabcabd", image="abcabd"))
